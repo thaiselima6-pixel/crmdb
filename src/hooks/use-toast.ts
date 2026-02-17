9df7c700dc4c14ebdd13237fac9559adbed70f71
thaiselima6-pixel <thaiselima6@gmail.com>
2026-02-17 09:22:48 -0300

    Fix toast typings by defining local types in use-toast
    Refactor type definitions for ToastActionElement and ToastProps.

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,10 +1,12 @@
 "use client";
 
 import * as React from "react";
-import type {
-  ToastActionElement,
-  ToastProps,
-} from "@/components/ui/toaster";
+
+type ToastActionElement = React.ReactElement | null;
+type ToastProps = {
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+};
 
 const TOAST_LIMIT = 1;
 const TOAST_REMOVE_DELAY = 1000000;
