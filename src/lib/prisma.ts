d986769b2d3e2b5a60267de701fc488cf1eab8a5
thaiselima6-pixel <thaiselima6@gmail.com>
2026-03-12 17:08:55 -0300

    Fix build: remove PrismaPg adapter and pg type conflicts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
 
 const prismaClientSingleton = () => {
-  const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
-  const adapter = new PrismaPg(pool);
-  return new PrismaClient({ adapter });
+  return new PrismaClient();
 };
 
 declare global {
